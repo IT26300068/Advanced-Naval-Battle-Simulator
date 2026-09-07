0e0f901ffcceb98a6b35ce4f9374b1378c7f519f
Vihanga Sathsara <IT26300068@my.Sliit.lk>
2026-09-07 23:32:53 +0530

    Contains function declarations used by the simulation

---
+++ simulation.h
@@ -0,0 +1,13 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "battlefield.h"
+
+void runPart1A(Battlefield *bf);
+void runPart1BSimulation1(Battlefield *bf);
+void runPart1BSimulation2(Battlefield *bf);
+void runPart1C(Battlefield *bf);
+void runPart2A(Battlefield *bf);
+void runPart2B(Battlefield *bf);
+
+#endif
